f77d1aa7adb641e2d599f760ecbb1adf450da32a
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-09-10 09:48:54 -0500

    use empty items array

--- fakeDB.js
+++ fakeDB.js
@@ -1,6 +1,8 @@
-global.items = [
-  { name: 'popsicle', price: 1.45 },
-  { name: 'cheerios', price: 3.4 },
-];
+// global.items = [
+//   { name: 'popsicle', price: 1.45 },
+//   { name: 'cheerios', price: 3.4 },
+// ];
+
+global.items = [];
 
 module.exports = items;
